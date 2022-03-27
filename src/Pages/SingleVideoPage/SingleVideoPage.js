fbdcd2c3c88352901ad46c78912ecfa1acdcbc11
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2022-03-27 16:52:55 +0530

    feat: added notes section

--- src/Pages/SingleVideoPage/SingleVideoPage.js
+++ src/Pages/SingleVideoPage/SingleVideoPage.js
@@ -11,21 +11,54 @@ function SingleVideoPage() {
             </iframe>
             
             <div class="single-video--info">
-                <p>Title | by User</p>
+                <h3>Title | by User</h3>
                 <div class="single-video--actions">
-                  <div>
-                    Views
+                  <div class="single-video--actions--container">
+                    <div class="single-video--action">
+                      <i class="far fa-thumbs-up"></i>
+                      <p class="margin-left--small">Like</p>
+                    </div>
+                    <div class="single-video--action">
+                      <i class="far fa-clock"></i>
+                      <p class="margin-left--small">Add to Watch Later</p>
+                    </div>
+                    <div class="single-video--action">
+                      <i class="fas fa-list"></i>
+                      <p class="margin-left--small">Add to Playlist</p>
+                    </div>
                   </div>
                   <div>
-                    Like
+                    Views | 4 hous ago
                   </div>
                 </div>
-                </div>
+            </div>
+
+            <div class="single-video--description">
+              <h3>Description</h3>
+              <p class="padding-tb--small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam at pellentesque ex. Etiam efficitur purus urna, quis rutrum lorem viverra nec. Phasellus augue sem, blandit a nibh sed, viverra consequat tortor. Nam posuere nibh quis nisi congue, vel rutrum dolor rutrum.</p>
+            </div>
           </div>
       </div>
-      <div>
-        <div class="padding--medium">
-          Notes
+      <div class="notes--section">
+        <div class="notes__container">
+            <div className="note__input--container margin-tb--large">          
+                <input onChange={null} value={null} className="note__input margin-right--medium" placeholder="note" />
+                <button onClick={null} className="btn btn--secondary">Submit</button>
+            </div> 
+            <div class="padding-top--small">
+                <div class="container__flex--spacebetween margin-tb--large">
+                  <p class="note--content">This is good</p>
+                  <i class="far fa-trash-alt"></i>
+                </div>
+                <div class="container__flex--spacebetween margin-tb--large">
+                  <p class="note--content">This is good, and bad at same time and lot more so keep improving</p>
+                  <i class="far fa-trash-alt"></i>
+                </div>
+                <div class="container__flex--spacebetween margin-tb--large">
+                  <p class="note--content">This is explained well</p>
+                  <i class="far fa-trash-alt"></i>
+                </div>
+            </div>
         </div>
       </div>
     </div>
