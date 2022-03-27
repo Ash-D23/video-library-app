93306fc914cffc21c653dd40cadaae4ebce52724
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2022-03-27 14:41:11 +0530

    feat: added single product page

--- src/Pages/SingleVideoPage/SingleVideoPage.js
+++ src/Pages/SingleVideoPage/SingleVideoPage.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import './SingleVideoPage.css'
 
 function SingleVideoPage() {
   return (
-    <div>SingleVideoPage</div>
+    <div className='single-video__container'>
+      <div class="single-video--content">
+          <div class="single-video">
+            
+            <iframe src="https://www.youtube.com/embed/tgbNymZ7vqY">
+            </iframe>
+            
+            <div class="single-video--info">
+                <p>Title | by User</p>
+                <div class="single-video--actions">
+                  <div>
+                    Views
+                  </div>
+                  <div>
+                    Like
+                  </div>
+                </div>
+                </div>
+          </div>
+      </div>
+      <div>
+        <div class="padding--medium">
+          Notes
+        </div>
+      </div>
+    </div>
   )
 }
 
